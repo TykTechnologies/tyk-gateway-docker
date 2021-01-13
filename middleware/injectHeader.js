e4b451fde0a7c0f8ca25036a9aa1c49b65349169
Sedky <sedkyaboushamalah@gmail.com>
2021-01-13 12:38:08 -0500

    add instructions for custom plugins

---
+++ middleware/injectHeader.js
@@ -0,0 +1,18 @@
+var testJSVMData = new TykJS.TykMiddleware.NewMiddleware({});
+
+testJSVMData.NewProcessRequest(function(request, session, config) {
+
+    console.log(JSON.stringify(request.Headers))
+    request.SetHeaders['custom-header'] = 'hello world';
+	return testJSVMData.ReturnData(request, {});
+});
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
